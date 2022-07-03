7549a58ce9a046871734addeb20e33357e38979d
Sumit Johri <firstcrushhome@gmail.com>
2022-07-03 20:07:58 +0100

    08 July 2022 Progress: Webview Alignment Fixed

--- app/src/main/java/co/firstcrush/firstcrush/HomeFragment.java
+++ app/src/main/java/co/firstcrush/firstcrush/HomeFragment.java
@@ -126,5 +126,11 @@ public class HomeFragment extends Fragment{
         return super.onOptionsItemSelected(item);
     }
 
-
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.getActivity().onWindowFocusChanged(hasFocus);
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
 }
